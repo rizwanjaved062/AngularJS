555cac2f415b1e830f5423eb4252d9bda83fc05f
Rizwan Javed <rizwan.javed@purelogics.net>
2020-01-01 22:27:50 +0500

    Completed Add, edit and Delete functionality

--- script.js
+++ script.js
@@ -2,21 +2,27 @@ angular.module('App', []).controller('CrudCtrl',
         function($scope, $http) {
 
             $http.get("http://localhost:8000/api/students")
-                    .then(function(response) {
+                    .then(function(response) { console.log(response.data);
                 $scope.Profiles = response.data;
             });
-
 
             $scope.entity = {}
 
             $scope.edit = function(index) {
                 $scope.entity = $scope.Profiles[index];
                 $scope.entity.index = index;
-                $scope.entity.editable = true;
+                $scope.entity.editable = true; 
+				$scope.recState = "editable";                				
             }
 
+            //delete Student record by Id
             $scope.delete = function(index) {
                 $scope.Profiles.splice(index, 1);
+				$http({
+                    url: 'http://localhost:8000/api/students/'+$scope.Profiles[index].id,
+                    method: "DELETE",
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                })
             }
 
             $scope.save = function(index) {
@@ -25,9 +31,18 @@ angular.module('App', []).controller('CrudCtrl',
                     name: $scope.Profiles[index].name,
                     course: $scope.Profiles[index].course
                 };
+				
+				if($scope.recState == 'new'){
+					$url = 'http://localhost:8000/api/students/create';
+					$method = "POST";
+				}else{
+                    $url = 'http://localhost:8000/api/students/'+$scope.Profiles[index].id;
+					$method = "PUT";
+                }				
+				
                 $http({
-                    url: 'http://localhost:8000/api/students/create',
-                    method: "POST",
+                    url: $url,
+                    method: $method,
                     data: Object.toparams(data),
                     headers: {'Content-Type': 'application/x-www-form-urlencoded'}
                 })
@@ -39,6 +54,7 @@ angular.module('App', []).controller('CrudCtrl',
                     country: "",
                     editable: true
                 })
+				$scope.recState = "new";
             }
 
             Object.toparams = function ObjecttoParams(obj) {
