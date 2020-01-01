aaf05b5bed3be1fd8e3949c52a4d1766b9c7449e
Rizwan Javed <rizwan.javed@purelogics.net>
2020-01-01 18:42:27 +0500

    Completed Student Create Method from Angular

--- script.js
+++ script.js
@@ -1,35 +1,56 @@
 angular.module('App', []).controller('CrudCtrl',
-	function($scope, $http) {
+        function($scope, $http) {
 
-  $http.get("http://localhost:8000/api/students")
-  .then(function(response) {
-      $scope.Profiles = response.data;
-  });
+            $http.get("http://localhost:8000/api/students")
+                    .then(function(response) {
+                $scope.Profiles = response.data;
+            });
 
-	 
-	 $scope.entity = {}
-	    
-	 $scope.edit = function(index){
-	   $scope.entity = $scope.Profiles[index];
-	   $scope.entity.index = index;
-	   $scope.entity.editable = true;
-	 }
-	    
-	 $scope.delete = function(index){
-	   $scope.Profiles.splice(index,1);
-	 }
-	    
-	 $scope.save = function(index){
-	   $scope.Profiles[index].editable = false;
-	   
-	 }
-	    
-	 $scope.add = function(){
-	   $scope.Profiles.push({
-	      name : "",
-        country : "",
-        editable : true
-	   })
-	 }
-	}
+
+            $scope.entity = {}
+
+            $scope.edit = function(index) {
+                $scope.entity = $scope.Profiles[index];
+                $scope.entity.index = index;
+                $scope.entity.editable = true;
+            }
+
+            $scope.delete = function(index) {
+                $scope.Profiles.splice(index, 1);
+            }
+
+            $scope.save = function(index) {
+                $scope.Profiles[index].editable = false;
+                var data = {
+                    name: $scope.Profiles[index].name,
+                    course: $scope.Profiles[index].course
+                };
+                $http({
+                    url: 'http://localhost:8000/api/students/create',
+                    method: "POST",
+                    data: Object.toparams(data),
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                })
+            }
+
+            $scope.add = function() {
+                $scope.Profiles.push({
+                    name: "",
+                    country: "",
+                    editable: true
+                })
+            }
+
+            Object.toparams = function ObjecttoParams(obj) {
+                var p = [];
+                for (var key in obj) {
+                    p.push(key + '=' + encodeURIComponent(obj[key]));
+                }
+                return p.join('&');
+            };
+
+        }
+
+
+
 );
